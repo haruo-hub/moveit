919bc70aca66275ee168b1ea4ef2926da401e1ff
Rodrigo Haruo <rodrigoharuosaitou@gmail.com>
2021-02-28 12:47:01 -0300

    Initial Commit Moveit

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect, useContext } from "react";
+import { start } from "repl";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import { CountdownContext } from "../contexts/CountdownContext";
+import styles from "../styles/components/Countdown.module.css";
+
+export function Countdown() {
+  const {
+    minutes,
+    seconds,
+    hasFinished,
+    isActive,
+    resetCountdown,
+    startCountdown,
+  } = useContext(CountdownContext);
+
+  const [minuteLef, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLef}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      {hasFinished ? (
+        <button disabled className={styles.countdownButton}>
+          Ciclo encerrado
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              onClick={resetCountdown}
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+            >
+              Abandonar um ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
+              onClick={startCountdown}
+              className={styles.countdownButton}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/haruo-hub.png" alt="Haruo" />
+      <div>
+        <strong>Haruo</strong>
+        <p><img src="icons/level.svg" alt=""/>Level {level}</p>
+      </div>
+    </div>
+  );
+}
